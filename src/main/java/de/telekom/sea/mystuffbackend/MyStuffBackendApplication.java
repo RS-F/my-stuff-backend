5ebec790138caeb5f4a91edddcf628b8acaa48e0
Ralf Schoch-Funkenhaus <ralf.schoch-funkenhaus@telekom.de>
2020-05-20 10:31:59 +0200

    Neustart: Initialer Commit

---
+++ src/main/java/de/telekom/sea/mystuffbackend/MyStuffBackendApplication.java
@@ -0,0 +1,13 @@
+package de.telekom.sea.mystuffbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStuffBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStuffBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/telekom/sea/mystuffbackend/MyStuffBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telekom.sea.mystuffbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStuffBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
